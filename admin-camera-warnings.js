04bbfefc91f5013188216d5b36c183d8eb20d5d2
Ryan Kelly <37750292+Armyrat60@users.noreply.github.com>
2025-08-31 00:27:27 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- admin-camera-warnings.js
+++ admin-camera-warnings.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 // Plugin version and repository information
-const PLUGIN_VERSION = 'v1.0.1';
+const PLUGIN_VERSION = 'v2.0.0';
 const GITHUB_OWNER = 'Armyrat60';
 const GITHUB_REPO = 'SquadJS-admin-camera-warnings';
 
@@ -1195,3 +1195,4 @@ export default class AdminCameraWarnings extends DiscordBasePlugin {
   // Auto-update functionality is now handled by the AutoUpdater utility
   // All update logic has been moved to squad-server/utils/auto-updater.js
 } 
+
